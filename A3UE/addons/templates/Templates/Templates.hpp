b3e4df9d263ecbdb0f9be66a7f29d1f71563b511
ramsey-droid <thunt501stramsey@gmail.com>
2025-03-17 19:56:04 +0000

    added mandoaaaa

--- A3UE/addons/templates/Templates/Templates.hpp
+++ A3UE/addons/templates/Templates/Templates.hpp
@@ -104,6 +104,18 @@ class Templates
         maps[] = {}; // If this template should be prioritized on any maps (case sensitive to worldName)
         description = "The Battle droid army"; // If this isn't included, no description will show (unless inherited from the base class.)
     };
+
+    class Mando : Vanilla_Base
+    {
+        basepath = QPATHTOFOLDER(Templates\SOB); //the path to the folder the template is located in, this translates to "\x\A3AE\addons\templates\Templates\Vanilla"
+        side = "Occ"; // Inv, Occ, Reb, Riv, Civ
+        flagTexture = "ls_statics_props\flags\data\flag_mandalorian.paa"; // Path to an icon to be displayed in the select menu.
+        name = "[SOB] Mandalorians"; // Name shown in the select menu.
+        file = "Mando"; // The template file name - .sqf, that gets appended automatically.
+        climate[] = {"arid", "temperate", "arctic", "tropical"}; // climate that the template can be selected on.
+        maps[] = {}; // If this template should be prioritized on any maps (case sensitive to worldName)
+        description = "The Battle droid army"; // If this isn't included, no description will show (unless inherited from the base class.)
+    };
 };
 
 
