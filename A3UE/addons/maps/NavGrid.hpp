b40413d590732d0db3e1b574759322b7ac3f9350
ramsey-droid <thunt501stramsey@gmail.com>
2025-09-11 22:24:31 +0100

    started work on tatooine

---
+++ A3UE/addons/maps/NavGrid.hpp
@@ -0,0 +1,4 @@
+class NavGrid { //overwrite for global
+    Altis = QPATHTOFOLDER(Antistasi_Altis.Altis\navGrid.sqf);
+    Tatooine = QPATHTOFOLDER(Antistasi_Tatooine.3as_tatooine\navGrid.sqf);
+};
---
+++ A3UE/addons/maps/mapInfo.hpp
@@ -0,0 +1,5 @@
+class mapInfo {
+	#include "Antistasi_Altis.Altis\mapInfo.hpp"
+	#include "Antistasi_Other_Altis.Altis\mapInfo.hpp"
+	#include "Antistasi_Tatooine.3as_tatooine\mapInfo.hpp"
+};
