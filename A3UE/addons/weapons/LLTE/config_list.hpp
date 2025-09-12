26e4d4e0ff991d05118431705ee2dfb0492b0949
ramsey-droid <thunt501stramsey@gmail.com>
2025-09-12 15:48:37 +0100

    made llte weapons compatible with IDA weapons

---
+++ A3UE/addons/weapons/LLTE/config_list.hpp
@@ -0,0 +1,1 @@
+#define WEAPON_LIST
--- A3UE/addons/weapons/SlotMacros.hpp
+++ A3UE/addons/weapons/SlotMacros.hpp
@@ -12,7 +12,14 @@
 "ls_cows_e11_optic",\
 "ls_cows_westar35s_scope",\
 "WM_E11_Optic",\
-"3AS_optic_DC15L_F"
+"3AS_optic_DC15L_F",\
+"JMSLLTE_a280_holo_Scope",\
+"JMSLLTE_dh17_holo_Scope",\
+"JMSLLTE_a180_bl_Scope",\
+"JMSLLTE_a280_bl_Scope",\
+"JMSLLTE_a300_bl_Scope",\
+"JMSLLTE_se14c_bl_Scope",\
+"JMSLLTE_se14r_bl_Scope"
 
 #define LONGSCOPES "3AS_Optic_Arkanian_2_F",\
 "3AS_optic_DC15LE_F",\
@@ -31,7 +38,19 @@
 "optic_DMS_weathered_Kir_F",\
 "optic_DMS_weathered_F",\
 "optic_KHS_old",\
-"optic_AMS"
+"optic_AMS",\
+"JMSLLTE_m45_bl_Scope",\
+"JMSLLTE_dlt19x_bl_Scope",\
+"JMSLLTE_rt97c_bl_Scope",\
+"JMSLLTE_m45_bl_Scope",\
+"JMSLLTE_e11_bl_Scope",\
+"JMSLLTE_e11_holo_Scope",\
+"JMSLLTE_e11_holoSF_Scope",\
+"JMSLLTE_DC15a_bl_Scope",\
+"JMSLLTE_dh17r_holo_Scope",\
+"JMSLLTE_ee3_bl_Scope",\
+"JMSLLTE_ee4_bl_Scope",\
+"JMSLLTE_ee4m_bl_Scope"
 
 #define SPECIALSCOPES "optic_Nightstalker",\
 "optic_NVS",\
@@ -57,6 +76,9 @@
 "3AS_Imp_Laser_DLT19",\
 "3AS_Imp_Light_E11",\
 "3AS_Imp_Laser_E11",\
+"JMSLLTE_acc_flashlight_reb",\
+"JMSLLTE_acc_flashlight",\
+
 
 #define BIPODS "bipod_03_F_blk",\
 "bipod_01_F_blk",\
@@ -66,4 +88,6 @@
 "3AS_Imp_Bipod_DLT19",\
 "3AS_bipod_VK38X_F",\
 "WM_DLT_Bipod",\
-"WM_E11_Pack"
+"WM_E11_Pack",\
+"JMSLLTE_FWMBk_bipod_F",\
+"JMSLLTE_FWMBk_leg_F",\
