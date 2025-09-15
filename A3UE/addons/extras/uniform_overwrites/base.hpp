393102f4f05377b2184d8c1c5a6ee61b87d19e08
ramsey-droid <thunt501stramsey@gmail.com>
2025-09-15 21:41:21 +0100

    made it so indfor can wear all opfor uniforms

--- A3UE/addons/extras/uniform_overwrites/base.hpp
+++ A3UE/addons/extras/uniform_overwrites/base.hpp
@@ -1,5 +1,8 @@
-class B_Soldier_F;
-class WM_StormBase: B_Soldier_F {
-    modelSides[] = {0, 1, 2, 3, 4, 5, 6, 7};
+class B_Soldier_base_F;
+class B_Soldier_F: B_Soldier_base_F {
+	modelSides[] = {0, 1, 2, 3, 4, 5, 6, 7};
 };
-
+class O_Soldier_base_F;
+class O_Soldier_F: O_Soldier_base_F {
+	modelSides[] = {0, 1, 2, 3, 4, 5, 6, 7};
+};
